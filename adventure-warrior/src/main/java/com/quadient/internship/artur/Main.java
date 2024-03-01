f1a489d10c1354b41447fbcc35e3eb7e4341823c
Andreas Lück <a.lueck@quadient.com>
2024-03-01 06:54:19 +0100

    Add warrior adventure game skeleton

---
+++ adventure-warrior/src/main/java/com/quadient/internship/artur/Main.java
@@ -0,0 +1,10 @@
+package com.quadient.internship.artur;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("""
+                Hello
+                World!
+                """);
+    }
+}
