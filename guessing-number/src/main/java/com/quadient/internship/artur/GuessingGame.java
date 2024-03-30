a34080b20e24c8a81f52bbbb902dc0793bd8e1c3
Artur Fleissner <fleissnerartur@gmail.com>
2024-03-30 16:20:49 +0100

    Fertig

--- guessing-number/src/main/java/com/quadient/internship/artur/GuessingGame.java
+++ guessing-number/src/main/java/com/quadient/internship/artur/GuessingGame.java
@@ -6,7 +6,15 @@ import com.quadient.internship.artur.utils.NumberUtils;
 public class GuessingGame {
     public int zufallsZahl;
 
-    public int versuche;
+    public int versuche = 10;
+
+    public void vorbei2() {
+        if (versuche <= 0) {
+            System.out.println("Du hast in 10 versuchen es nicht geschafft die Zahl " + zufallsZahl + (" herausgefunden!"));
+            vorbei();
+        }
+
+    }
 
     public void generiereZufallszahl() {
         zufallsZahl = NumberUtils.random(1, 101);
@@ -15,19 +23,20 @@ public class GuessingGame {
     public void benutzereingabe() {
         int x = InputUtils.readIntegerInput();
         ueberpruefeBenutzereingabe(x);
+        versuche--;
     }
 
     public void ueberpruefeBenutzereingabe(final int eingabe) {
-        /*Überprüfen ob Zahl >||< ist und ggf. Tipps geben*/
         if (eingabe < zufallsZahl) {
             System.out.println("Deine Zahl ist zu klein!");
+            vorbei2();
         } else if (eingabe > zufallsZahl) {
             System.out.println("Deine Zahl ist zu groß!");
+            vorbei2();
         } else {
             System.out.println("Du hasst die Zahl " + zufallsZahl + " heraus gefunden!");
             vorbei();
         }
-
     }
 
     public void vorbei() {
--- guessing-number/src/main/java/com/quadient/internship/artur/Main.java
+++ guessing-number/src/main/java/com/quadient/internship/artur/Main.java
@@ -2,9 +2,10 @@ package com.quadient.internship.artur;
 
 public class Main {
     public static void main(final String[] args) {
+
         for (int t = 10; t >= 0; t--) {
             System.out.println("Errate die Zahl zwischen 1 und 100!");
-            GuessingGame();
+
             /* Objekt verwendet werden
              * 10 Versuche*/
         }
