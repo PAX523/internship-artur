accffd5b865bdb62e0a93bb8acbc3de71dc08b44
Andreas Lück <a.lueck@quadient.com>
2024-04-26 10:39:33 +0200

    Provide graphics for different layouts

--- swing/src/main/java/com/quadient/internship/artur/Main.java
+++ swing/src/main/java/com/quadient/internship/artur/Main.java
@@ -2,6 +2,7 @@ package com.quadient.internship.artur;
 
 public class Main {
     public static void main(final String[] args) {
+        // https://www.java-tutorial.org/layout-manager.html
         System.out.println("Hallo Welt");
     }
 }
