0722d99fff573819ef939bf666965d847d452f55
Andreas Lück <a.lueck@quadient.com>
2024-04-26 10:25:17 +0200

    Prepare exercise for threads

---
+++ swing/src/main/java/com/quadient/internship/artur/threads/ThreadsMain.java
@@ -0,0 +1,8 @@
+package com.quadient.internship.artur.threads;
+
+public class ThreadsMain {
+    public static void main(final String[] args) {
+        System.err.println("Hallo Welt!");
+    }
+}
+
